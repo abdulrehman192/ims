3e795444800b41aa718668d34170adfad06561c0
abdulrehman192 <mr.abdulrehman.ar@gmail.com>
2024-05-09 16:43:30 +0500

    update apis

--- config/database.js
+++ config/database.js
@@ -1,18 +1,37 @@
-// need to install mysql
-
 const { createPool } = require("mysql");
 
-const pool = createPool({
-    port : process.env.DB_PORT,
-    host : process.env.DB_HOST,
-    user : process.env.DB_USER,
-    password : process.env.DB_PASS,
-    database : process.env.MYSQL_DB,
-    connectionLimit : 1000,
-    connectTimeout  : 60 * 60 * 1000,
-    acquireTimeout  : 60 * 60 * 1000,
-    timeout         : 60 * 60 * 1000,
-}); 
+let pool;
 
+// Check if the authentication mode is native
+if (process.env.DB_AUTH_MODE === 'native') {
+    pool = createPool({
+        port: 3306,
+        host: 'srv1144.hstgr.io',
+        user: 'u438935127_ims',
+        password: 'Rehman92$!',
+        database: 'u438935127_ims',
+        connectionLimit: 1000,
+        connectTimeout: 60 * 60 * 1000,
+        acquireTimeout: 60 * 60 * 1000,
+        timeout: 60 * 60 * 1000,
+    });
+  
+} else {
+    // Connect to the MySQL server using non-native authentication mode
+    pool = createPool({
+        port: process.env.DB_PORT,
+        host: 'srv1144.hstgr.io',
+        user: 'u438935127_ims',
+        password: 'Rehman92$!',
+        database: 'u438935127_ims',
+        connectionLimit: 1000,
+        connectTimeout: 60 * 60 * 1000,
+        acquireTimeout: 60 * 60 * 1000,
+        timeout: 60 * 60 * 1000,
+        authPlugins: {
+            mysql_clear_password: () => () => Buffer.from(process.env.DB_PASS + '\0')
+        }
+    });
+}
 
 module.exports = pool;
